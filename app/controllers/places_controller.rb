452310af9a44bf20f3576efd8061f4434a3df14b
spinn123 <sarah.pinner@kellogg.northwestern.edu>
2022-05-22 12:03:33 +0000

    fixes ruby version, added view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,6 @@
 class PlacesController < ApplicationController
 
-def index
-    
-end
+    def index
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
     resources "places"
   end
 
-# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+# get("/", { :controller => "articles", :action => "index" })
