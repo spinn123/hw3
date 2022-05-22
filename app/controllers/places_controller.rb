8c4ba8401aed9afe984222b7c885eff6a6badc75
spinn123 <sarah.pinner@kellogg.northwestern.edu>
2022-05-22 12:25:13 +0000

    moved ruby to controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,15 @@
 class PlacesController < ApplicationController
 
     def index
+        @places = Place.all
     end
+
+    def create
+    end
+
+    def new
+        @place = Place.new
+    end
+
 
 end
