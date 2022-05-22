8270cfe53f8ab0aff2638af98264204d741902e9
spinn123 <sarah.pinner@kellogg.northwestern.edu>
2022-05-22 12:57:02 +0000

    Get Italy to show

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,12 +4,19 @@ class PlacesController < ApplicationController
         @places = Place.all
     end
 
+
     def create
+        @place = Place.new(params["name"])
+        @place.save
+        redirect_to "/places"
     end
 
     def new
         @place = Place.new
     end
 
+    def show
+        @place = Place.find_by({ "id" => params["id"] })
+      end
 
 end
