4aa48c001a8a460009abc3cf92366a925463e9c6
spinn123 <sarah.pinner@kellogg.northwestern.edu>
2022-05-22 14:06:18 +0000

    working on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,14 @@ class PostsController < ApplicationController
         @posts = Post.all
     end
 
+    def new
+        @post = Post.new
+        @post.place_id = params["place_id"]
+      end
 
-
-
+      def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places/#{@post.place.id}"
+      end
 end
